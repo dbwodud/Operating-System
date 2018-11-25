37217327ead1dbad27f21a6ea31c864a3514fbce
dbwodud <dbwodud34@gmail.com>
2018-11-25 13:01:36 +0900

    명령어 실행(디버깅 필요)

--- Chapter3/youShell.c
+++ Chapter3/youShell.c
@@ -1,18 +1,27 @@
 #include<sys/types.h>
 #include<sys/wait.h>
-
+#include<string.h>
 #include<stdio.h>
 #include<unistd.h>
+#include<errno.h>
 
 #define MAX_LINE 80
-
 int main(void){
-    char *args[MAX_LINE/2+1];
+    char *args[MAX_LINE/2+1]={NULL};
     int should_run = 1;
 
     while(should_run){
         printf("you>");
         fflush(stdout);
+        int i;
+        char *original;
+        fgets(original,MAX_LINE/2+1,stdin);
+        char *command = strtok(original," ");
+        i=0;
+        while(command!=NULL){
+            args[i++]=command;	
+            command = strtok(NULL," ");
+        }
         pid_t pid;
         pid = fork();
         if(pid<0){
@@ -21,11 +30,15 @@ int main(void){
         }
         //Child Process
         else if(pid==0){
+            printf("%s",args[0]);
+            if(args[1]==NULL){
+                printf("NULL");
+            }
             execvp(args[0],args);
+            printf("command is not valid\n");
         }
         //Parent Process
         else{
-
             wait(NULL);
         }
     }
