ac5132585afc4f3d4c48f59cb2055d0482c7da3a
dbwodud <dbwodud34@gmail.com>
2018-11-26 13:30:15 +0900

    Debug

--- Chapter3/youShell.c
+++ Chapter3/youShell.c
@@ -4,36 +4,72 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<errno.h>
+#include<stdlib.h>
 
 #define MAX_LINE 80
+
+typedef char* string;
+typedef struct history{
+    char str[80];
+    struct history *next;
+    struct history *before;
+}history;
+
+history* head;
+history* cur; 
+history* tail;
+
+void init(){
+    head = malloc(sizeof(history));
+    tail = cur = head;
+}
+
+void push_back(string str){
+    strcpy(cur->str,str);
+
+}
+
 int main(void){
-    char *args[MAX_LINE/2+1]={NULL};
+    
+    string args[MAX_LINE/2+1]={NULL,};
     int should_run = 1;
+    init();
 
     while(should_run){
         printf("you>");
         fflush(stdout);
-        int i;
-        char *original;
+        
+        int i=0;
+        string original;
+
         fgets(original,MAX_LINE/2+1,stdin);
-        char *command = strtok(original," ");
-        i=0;
-        while(command!=NULL){
-            args[i++]=command;	
-            command = strtok(NULL," ");
+
+        string command = strtok(original," \n");
+        if(!command){
+            continue;
         }
-        pid_t pid;
-        pid = fork();
+
+        if(!strcmp("history",command)){
+            continue;
+        }
+        else if(!strcmp("!!",command)){
+            continue;
+        }
+        else if(!strcmp("!",command)){
+            continue;
+        }
+
+        while(command){
+            args[i++]=command;	
+            command = strtok(NULL," \n");
+        }
+        pid_t pid=fork();
         if(pid<0){
             fprintf(stderr,"Fork Error");
             return 1;
         }
         //Child Process
-        else if(pid==0){
-            printf("%s",args[0]);
-            if(args[1]==NULL){
-                printf("NULL");
-            }
+        else if(!pid){
             execvp(args[0],args);
             printf("command is not valid\n");
         }
